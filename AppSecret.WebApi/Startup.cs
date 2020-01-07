c1473e6be90ad190ee97b2faa50ba6c819b1d18f
William Niedzwiedz <ext.william.silva@farfetch.com>
2020-01-07 15:05:20 +0000

    set swagger auth

--- AppSecret.WebApi/Startup.cs
+++ AppSecret.WebApi/Startup.cs
@@ -67,6 +67,27 @@ namespace AppSecret.WebApi
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "AppSecrect", Version = "v1" });
+
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    In = ParameterLocation.Header,
+                    Description = "Please insert JWT with Bearer into field",
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey
+                });
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement {
+                 {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        }
+                    },
+                    new string[] { }
+                }
+            });
             });
 
             services.AddAuthentication(x =>
